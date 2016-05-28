efb173b79e018aebfb82e2853d58065165765020
lazybios <lazybios@gmail.com>
2016-05-28 20:42:33 +0800

    feat(framework): init menubar framwork

---
+++ index.js
@@ -0,0 +1,18 @@
+var menubar = require('menubar')
+
+var mb = menubar({
+  width: 300,
+  height: 300,
+  icon: 'icon.png'
+})
+
+mb.on('ready', function ready(){
+  console.log('app is ready!!');
+})
+
+mb.on('after-create-window', function (){
+
+  mb.window.openDevTools()
+  mb.window.loadURL(`file://${__dirname}/index.html`)
+
+})
