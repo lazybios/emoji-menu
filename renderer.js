c1bd1b6f9bd4d6e9ee5d7fef45711f87f16f27cd
runfastlynda <runfastlynda@gmail.com>
2016-05-29 22:17:11 +0800

    feat(renderer): paginate emoji group and copy status tips

--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+//Copy
+
 var Clipboard = require(`${__dirname}/node_modules/clipboard/dist/clipboard.js`)
 
 var clipboard = new Clipboard(".emoji", {
@@ -9,28 +11,77 @@ var clipboard = new Clipboard(".emoji", {
 clipboard.on('success', function(e){
   // toast copy success
   console.info('Action:', e.action);
+  tipsMessage('Copy Success');
   console.info('Text:', e.text);
   console.info('Trigger:', e.trigger);
-
   e.clearSelection();
 });
 
 clipboard.on('error', function(e){
   // toast copy failed
+  tipsMessage('Copy Failed');
+
   console.error('Action:', e.action);
   console.error('Trigger:', e.trigger);
 });
 
-var tabs=document.getElementById('emoji-tab').getElementsByTagName('li');
-var divs=document.getElementById('emoji-lists').getElementsByTagName('div');
-for (var i = 0; i < tabs.length; i++) {
-  tabs[i].id=i;
-  tabs[i].onclick=function(){
-    for (var j = 0; j < tabs.length; j++) {
-      tabs[j].className='';
-      divs[j].style.display='none';
+function tipsMessage(text){
+  var tips = document.getElementById('emoji-tips');
+  tips.innerHTML = text;
+  tips.style.display = 'block';
+  var tipsDisappear = setTimeout(function(){
+    tips.style.display = 'none';
+  },2000);
+}
+
+
+//Tab
+
+var emojiTabs = document.getElementById('emoji-tab').getElementsByTagName('li');
+var emojiLists = document.getElementById('emoji-lists').getElementsByTagName('div');
+for (var i = 0; i < emojiTabs.length; i++) {
+  emojiTabs[i].id = i;
+  emojiTabs[i].onclick = function(){
+    for (var j = 0; j < emojiTabs.length; j++) {
+      emojiTabs[j].className = '';
+      emojiLists[j].style.display = 'none';
     }
-    this.className='select';
-    divs[this.id].style.display='block';
+    this.className='select-tab';
+    emojiLists[this.id].style.display = 'block';
   }
 }
+
+//Lists Number
+
+var peoplesSections = document.getElementById('peoples').getElementsByTagName('section');
+var peoplesListNumber = document.getElementById('peoples-list-number').getElementsByTagName('li');
+var naturesSections = document.getElementById('natures').getElementsByTagName('section');
+var naturesListNumber = document.getElementById('natures-list-number').getElementsByTagName('li');
+var objectsSections = document.getElementById('objects').getElementsByTagName('section');
+var objectsListNumber = document.getElementById('objects-list-number').getElementsByTagName('li');
+var placesSections = document.getElementById('places').getElementsByTagName('section');
+var placesListNumber = document.getElementById('places-list-number').getElementsByTagName('li');
+var symbolsSections = document.getElementById('symbols').getElementsByTagName('section');
+var symbolsListNumber = document.getElementById('symbols-list-number').getElementsByTagName('li');
+
+function changeSection(tab,div){
+  for (var i = 0; i < tab.length; i++) {
+    tab[i].id = i;
+    tab[i].onmouseover = function(){
+      for (var j = 0; j < tab.length; j++) {
+        tab[j].className = '';
+        div[j].style.display = 'none';
+      }
+      this.className='select';
+      div[this.id].style.display = 'block';
+    }
+  }
+}
+
+changeSection(peoplesListNumber,peoplesSections);
+changeSection(naturesListNumber,naturesSections);
+changeSection(objectsListNumber,objectsSections);
+changeSection(placesListNumber,placesSections);
+changeSection(symbolsListNumber,symbolsSections);
+
+
