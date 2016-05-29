bce0e9aa5527d5c1569447232be128ab8338ed78
runfastlynda <runfastlynda@gmail.com>
2016-05-29 23:47:09 +0800

    style(renderer): correction code style

--- renderer.js
+++ renderer.js
@@ -1,5 +1,5 @@
-//Copy
 
+//Copy
 var Clipboard = require(`${__dirname}/node_modules/clipboard/dist/clipboard.js`)
 
 var clipboard = new Clipboard(".emoji", {
@@ -9,34 +9,28 @@ var clipboard = new Clipboard(".emoji", {
 });
 
 clipboard.on('success', function(e){
+
   // toast copy success
-  console.info('Action:', e.action);
-  tipsMessage('Copy Success');
-  console.info('Text:', e.text);
-  console.info('Trigger:', e.trigger);
+  statusTips('Copy Success');
   e.clearSelection();
 });
 
 clipboard.on('error', function(e){
-  // toast copy failed
-  tipsMessage('Copy Failed');
 
-  console.error('Action:', e.action);
-  console.error('Trigger:', e.trigger);
+  // toast copy failed
+  statusTips('Copy Failed');
 });
 
-function tipsMessage(text){
-  var tips = document.getElementById('emoji-tips');
-  tips.innerHTML = text;
-  tips.style.display = 'block';
-  var tipsDisappear = setTimeout(function(){
-    tips.style.display = 'none';
+function statusTips(text){
+  var CopyStatus = document.getElementById('emoji-copy-status');
+  CopyStatus.innerHTML = text;
+  CopyStatus.style.display = 'block';
+  var CopyStatusDisappear = setTimeout(function(){
+    CopyStatus.style.display = 'none';
   },2000);
 }
 
-
 //Tab
-
 var emojiTabs = document.getElementById('emoji-tab').getElementsByTagName('li');
 var emojiLists = document.getElementById('emoji-lists').getElementsByTagName('div');
 for (var i = 0; i < emojiTabs.length; i++) {
@@ -46,13 +40,12 @@ for (var i = 0; i < emojiTabs.length; i++) {
       emojiTabs[j].className = '';
       emojiLists[j].style.display = 'none';
     }
-    this.className='select-tab';
+    this.className='tab-selected';
     emojiLists[this.id].style.display = 'block';
   }
 }
 
 //Lists Number
-
 var peoplesSections = document.getElementById('peoples').getElementsByTagName('section');
 var peoplesListNumber = document.getElementById('peoples-list-number').getElementsByTagName('li');
 var naturesSections = document.getElementById('natures').getElementsByTagName('section');
@@ -72,7 +65,7 @@ function changeSection(tab,div){
         tab[j].className = '';
         div[j].style.display = 'none';
       }
-      this.className='select';
+      this.className='pagination-selected';
       div[this.id].style.display = 'block';
     }
   }
