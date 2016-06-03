8ae5489a70a4a3ab8c4b7256f88c047690468cbf
runfastlynda <runfastlynda@gmail.com>
2016-06-03 14:15:45 +0800

    style(renderer): remove unnecessary white-space

--- renderer.js
+++ renderer.js
@@ -5,16 +5,16 @@ var $ = require("jquery");
 // Pagination
 function pagination(group, i){
   var imgs = $(group+' img');
-  imgs.each(function (index, el) {
-    if (index >= i * 70 && index < ((+i + 1) * 70)) {
+  imgs.each(function(index, el){
+    if(index >= i * 70 && index < ((+i + 1) * 70)){
       $(el).show();
-    } else {
+    }else{
       $(el).hide();
     }
   });
 }
 
-function highlightAll() {
+function highlightAll(){
   pagination('#people', 0);
   pagination('#nature', 0);
   pagination('#objects', 0);
@@ -28,20 +28,20 @@ highlightAll();
 // Copy
 var Clipboard = require(`${__dirname}/node_modules/clipboard/dist/clipboard.js`);
 
-var clipboard = new Clipboard(".emoji-cell", {
-  text: function (trigger) {
+var clipboard = new Clipboard('.emoji-cell', {
+  text: function(trigger){
     return trigger.getAttribute('alt');
   }
 });
 
-clipboard.on('success', function (e) {
+clipboard.on('success', function(e){
 
   // Toast copy success
   statusTips('Copied!');
   e.clearSelection();
 });
 
-clipboard.on('error', function (e) {
+clipboard.on('error', function(e){
 
   // Toast copy failed
   statusTips('Copy Failed');
@@ -56,12 +56,12 @@ function statusTips(text){
 // Tab
 var emojiTabs = $('#emoji-tab>li');
 var emojiLists = $('#emoji-view>div');
-$('#emoji-tab li').on('click', function () {
+$('#emoji-tab li').on('click', function(){
   emojiTabs.removeClass('tab-selected');
   emojiLists.hide();
   $(this).addClass('tab-selected');
-  for (var i = 0; i < emojiLists.length; i++) {
-    if ($(this).text().toLowerCase() === emojiLists[i].id) {
+  for(var i = 0; i < emojiLists.length; i++){
+    if($(this).text().toLowerCase() === emojiLists[i].id){
       $('#'+$(this).text().toLowerCase()).show();
     }
   }
@@ -75,9 +75,9 @@ var placesListNumber = $('#places-list-number>li');
 var symbolsListNumber = $('#symbols-list-number>li');
 
 function hoverChangeSection(tab){
-  for (var i = 0; i < tab.length; i++) {
+  for(var i = 0; i < tab.length; i++){
     $(tab).eq(i).attr('data-text', i);
-    $(tab).eq(i).on('mouseover', function () {
+    $(tab).eq(i).on('mouseover', function(){
       $(tab).removeClass('pagination-selected');
       $(this).addClass('pagination-selected');
       var selectedGroup = '#'+$('.tab-selected').text().toLowerCase();
@@ -99,15 +99,15 @@ hoverChangeSection(placesListNumber);
 hoverChangeSection(symbolsListNumber);
 
 // Search
-function isElementMatching(element, needle) {
+function isElementMatching(element, needle){
   var alternative = element.attr('data-alternative-name');
   var name = element.attr('alt');
   return (name.toLowerCase().indexOf(needle) >= 0) ||
     (alternative != null && alternative.toLowerCase().indexOf(needle) >= 0);
 }
 
-function highlightElements(needle) {
-  if (needle.length == 0) {
+function highlightElements(needle){
+  if(needle.length == 0){
     highlightAll();
     $('#emoji-search-delete').hide();
     return;
@@ -121,31 +121,31 @@ function highlightElements(needle) {
   initPages(symbolsListNumber);
   
   needle = needle.toLowerCase();
-  $('#emoji-view img').each(function (index, el) {
-    if (isElementMatching($(el), needle)) {
+  $('#emoji-view img').each(function(index, el){
+    if(isElementMatching($(el), needle)){
       $(el).show();
-    } else {
+    }else{
       $(el).hide();
     }
   });
 }
 
-$('#emoji-search>input').keyup(function (e) {
-  if (e.keyCode == 27) {
+$('#emoji-search>input').keyup(function(e){
+  if(e.keyCode == 27){
     $(this).val('').blur();
     highlightAll();
     $('#emoji-search-delete').hide();
   }
 });
 
-$('#emoji-search>input').on('change paste keyup', function () {
+$('#emoji-search>input').on('change paste keyup', function(){
   $('#emoji-search-delete').show();
   highlightElements($('#emoji-search>input').val());
 });
 
 $('#emoji-search>input').focus();
 
-$('#emoji-search-delete').on('click', function () {
+$('#emoji-search-delete').on('click', function(){
   $('#emoji-search>input').val('');
   $('#emoji-search-delete').hide();
   highlightAll();
